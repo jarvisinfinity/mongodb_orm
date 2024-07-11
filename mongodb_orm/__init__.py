db74c6a2b7d962d05c539aa6bb2dbbadc1d0918d
Tony Stark <tonystark.jarvis.infinity@gmail.com>
2024-07-11 10:38:57 +0530

    Update: Fix async and await

--- mongodb_orm/__init__.py
+++ mongodb_orm/__init__.py
@@ -1,3 +1,1 @@
-from typing import ClassVar, Optional, List, Any
-
 __version__ = '1.0.0'
--- mongodb_orm/models.py
+++ mongodb_orm/models.py
@@ -42,9 +42,9 @@ class BaseModel(pydantic.BaseModel):
     def json(self):
         return super().model_dump_json()
 
-    def get_id(self):
+    async def get_id(self):
         if not self.id:
-            sequence = self.id_sequences.find_one_and_update(
+            sequence = await self.id_sequences.find_one_and_update(
                 {"_id": self.collection_name},
                 {"$inc": {"seq": 1}},
                 upsert=True,
@@ -54,12 +54,12 @@ class BaseModel(pydantic.BaseModel):
             return self.id
 
     @classmethod
-    def get(cls, **kwargs):
-        resp = cls.collection.find_one(kwargs)
+    async def get(cls, **kwargs):
+        resp = await cls.collection.find_one(kwargs)
         return cls(**resp) if resp else None
 
     @classmethod
-    def filter(cls, **kwargs):
+    async def filter(cls, **kwargs):
         sort_by = kwargs.pop("sort_by", {"id": 1})
         distinct = kwargs.pop("distinct", None)
         only_count = kwargs.pop("only_count", False)
@@ -75,68 +75,68 @@ class BaseModel(pydantic.BaseModel):
         if only_count:
             return len(list(responses))
         if len(projection) == 1 and ("_id" in projection.keys()):
-            resp = [cls(**resp) for resp in responses] if responses else []
+            resp = [await cls(**resp) for resp in responses] if responses else []
         elif len(projection) == 2 and ("_id" in projection.keys()) and flat_projection:
             projection.pop("_id")
             key = list(projection.keys())[0]
-            resp = [resp[key] for resp in responses] if responses else []
+            resp = [await resp[key] for resp in responses] if responses else []
         else:
-            resp = [resp for resp in responses] if responses else []
+            resp = [await resp for resp in responses] if responses else []
         return resp
 
     @classmethod
-    def all(cls):
-        return cls.filter()
+    async def all(cls):
+        return await cls.filter()
 
     @classmethod
-    def create(cls, **kwargs):
+    async def create(cls, **kwargs):
         self = cls(**kwargs)
         if not self.id:
-            self.id = self.get_id()
-        result = cls.collection.insert_one(self.dict())
-        return self.get(_id=result.inserted_id)
+            self.id = await self.get_id()
+        result = await cls.collection.insert_one(self.dict())
+        return await self.get(_id=result.inserted_id)
 
     @classmethod
-    def get_or_create(cls, **kwargs):
-        resp = cls.get(**kwargs)
+    async def get_or_create(cls, **kwargs):
+        resp = await cls.get(**kwargs)
         if resp:
             return (resp, False)
-        return (cls.create(**kwargs), True)
+        return (await cls.create(**kwargs), True)
 
-    def save(self, only_update=False):
+    async def save(self, only_update=False):
         if not self.id:
             if only_update:
                 return False
             else:
-                self.id = self.get_id()
-        self.collection.update_one({"id": self.id}, {"$set": self.dict()}, upsert=not only_update)
+                self.id = await self.get_id()
+        await self.collection.update_one({"id": self.id}, {"$set": self.dict()}, upsert=not only_update)
         return self
 
-    def delete(self):
-        self.collection.delete_one({"id": self.id})
+    async def delete(self):
+        await self.collection.delete_one({"id": self.id})
         return True
 
     @classmethod
-    def aggregate(cls, *args, **kwargs):
-        data = cls.collection.aggregate(*args, **kwargs)
+    async def aggregate(cls, *args, **kwargs):
+        data = await cls.collection.aggregate(*args, **kwargs)
         return list(data) if data else None
 
     @classmethod
-    def make_unique(cls, field, order=pymongo.ASCENDING):
+    async def make_unique(cls, field, order=pymongo.ASCENDING):
         contraint_key = field + "_" + "1" if order == pymongo.ASCENDING else "-1"
-        if contraint_key not in cls.collection.index_information():
-            print('Index Added', cls.collection.create_index(field, unique=True))
+        if contraint_key not in await cls.collection.index_information():
+            print('Index Added', await cls.collection.create_index(field, unique=True))
         else:
             print('Index already exists')
 
     @classmethod
-    def make_unique_together(cls, fields):
+    async def make_unique_together(cls, fields):
         constraints = []
         contraint_keys = []
         for key, value in fields.items():
             constraints.append((key, value))
             contraint_keys.append(key + "_" + "1" if value == pymongo.ASCENDING else "-1")
-            if "_".join(contraint_keys) not in cls.collection.index_information():
-                print('Index Added', cls.collection.create_index(constraints, unique=value))
+            if "_".join(contraint_keys) not in await cls.collection.index_information():
+                print('Index Added', await cls.collection.create_index(constraints, unique=value))
             else:
                 print('Index already exists')
